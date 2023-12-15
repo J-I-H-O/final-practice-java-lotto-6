27a29bf24ed930b4b47ab4da5b13eb7061452e7c
J-I-H-O <jeongjiho0731@gmail.com>
2023-12-15 12:21:41 +0900

    feat(PurchasePrice): 구매 금액 검증 코드 작성

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,9 +1,21 @@
 package lotto.controller;
 
+import lotto.domain.PurchasePrice;
 import lotto.view.InputView;
 
 public class MainController {
     public void run() {
-        String inputPrice = InputView.inputPurchasePrice();
+        PurchasePrice price = purchase();
+    }
+
+    private PurchasePrice purchase() {
+        while (true) {
+            try {
+                String inputPrice = InputView.inputPurchasePrice();
+                return new PurchasePrice(inputPrice);
+            } catch (IllegalArgumentException e) {
+                System.out.println("\n" + e.getMessage() + "\n");
+            }
+        }
     }
 }
---
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+public class PurchasePrice {
+    private static final String NOT_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력할 수 없습니다.";
+    private static final String MIN_PURCHASE_PRICE_ERROR_MESSAGE = "[ERROR] 1000원 이상의 금액만 입력할 수 있습니다.";
+    private static final String NOT_DIVISIBLE_ERROR_MESSAGE = "[ERROR] 1000원 단위로만 입력할 수 있습니다.";
+    private static final int PURCHASABLE_CURRENCY_UNIT = 1000;
+
+    private final int money;
+
+    public PurchasePrice(String moneyInput) {
+        validateNumeric(moneyInput);
+        int money = Integer.parseInt(moneyInput);
+        validateMinimumPrice(money);
+        validateDivisibility(money);
+
+        this.money = money;
+    }
+
+    private void validateNumeric(String moneyInput) {
+        try {
+            Integer.parseInt(moneyInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateMinimumPrice(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException(MIN_PURCHASE_PRICE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDivisibility(int money) {
+        if (money % PURCHASABLE_CURRENCY_UNIT != 0) {
+            throw new IllegalArgumentException(NOT_DIVISIBLE_ERROR_MESSAGE);
+        }
+    }
+}
