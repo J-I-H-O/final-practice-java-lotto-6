c8cceb47a952dced9692453c001bd3d8b10119f1
J-I-H-O <jeongjiho0731@gmail.com>
2023-12-15 11:55:17 +0900

    feat: 구입 금액 입력 기능 초기 개발

---
+++ src/main/java/lotto/controller/MainController.java
@@ -0,0 +1,9 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class MainController {
+    public void run() {
+        String inputPrice = InputView.inputPurchasePrice();
+    }
+}
